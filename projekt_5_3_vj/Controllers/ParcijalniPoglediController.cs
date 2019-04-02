2c1c04f4ec6db58b0ae68a11e71263d8aaded167
Antun Majdančić <tuna.gfos@gmail.com>
2019-04-02 18:31:09 +0200

    partial

---
+++ projekt_5_3_vj/Controllers/ParcijalniPoglediController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_5_3_vj.Controllers
+{
+    public class ParcijalniPoglediController : Controller
+    {
+        // GET: ParcijalniPogledi
+        public ViewResult PrikaziKosaricu()
+        {
+            List<Artikal> ListaArtikala = new List<Artikal>()
+            {
+                new Artikal(){ Cijena=9.99m, Kategorija="plava", Kolicina=5, Naziv="Keks"},
+                new Artikal(){ Cijena=8.99m, Kategorija="zelena", Kolicina=7, Naziv="Kifla"},
+                new Artikal(){ Cijena=6.99m, Kategorija="zuta", Kolicina=20, Naziv="Kroki"}
+            };
+            return View(ListaArtikala);
+        }
+    }
+
+    internal class Artikal
+    {
+    }
+}
