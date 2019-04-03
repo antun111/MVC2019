ce81d53282e49b1aeeb1f51014f8a79996f4c260
Antun Majdančić <tuna.gfos@gmail.com>
2019-04-03 18:37:57 +0200

    POS

---
+++ projekt_5_3_vj/Controllers/HtmlHelperiController.cs
@@ -0,0 +1,34 @@
+﻿using projekt_5_3_vj.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_5_3_vj.Controllers
+{
+    public class HtmlHelperiController : Controller
+    {
+        // GET: HtmlHelperi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private string[] mjesta = new string[] { "Split", "Osijek", "Rijeka", "Zagreb" };
+
+        public ViewResult FormHelper()
+        {
+            ViewBag.Mjesta = mjesta;
+            return View(new Osoba());
+        }
+
+        [HttpPost]
+        public ViewResult FormHelper(Osoba osoba)
+        {
+            ViewBag.Mjesta = this.mjesta;
+            ViewBag.Poruka = "podaci uneseni!";
+            return View(new Osoba());
+        }
+    }
+}
