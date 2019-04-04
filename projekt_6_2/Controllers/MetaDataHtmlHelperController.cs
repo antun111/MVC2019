db8bb0070cc7df61ade32e7298557691270bdb74
Antun Majdančić <tuna.gfos@gmail.com>
2019-04-04 18:41:36 +0200

    metapodaci

---
+++ projekt_6_2/Controllers/MetaDataHtmlHelperController.cs
@@ -0,0 +1,24 @@
+﻿using projekt_6_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace projekt_6_2.Controllers
+{
+    public class MetaDataHtmlHelperController : Controller
+    {
+        // GET: MetaDataHtmlHelper
+        public ActionResult MetaDataView()
+        {
+            return View(new OsobaMeta());
+        }
+        [HttpPost]
+        public ActionResult MetaDataView(OsobaMeta osoba)
+        {
+            return View("HtmlLabelDisplay", osoba);
+        }
+    }
+}
