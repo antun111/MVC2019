94aadd0d195b3ce47e3362b810f6a55cc1b5e689
Antun Majdančić <tuna.gfos@gmail.com>
2019-04-05 17:34:23 +0200

    validacije

---
+++ validacije/Controllers/ValidacijeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using validacije.Models;
+
+namespace validacije.Controllers
+{
+    public class ValidacijeController : Controller
+    {
+        // GET: Validacije
+        public ViewResult IzdavanjeRacuna()
+        {
+            return View(new Racun() { Datum = DateTime.Now, BrojRacuna = "/" + DateTime.Now.Year.ToString() });
+        }
+
+        [HttpPost]
+        public ViewResult IzdavanjeRacuna(Racun racun, string id)
+        {
+            //obavezne vrijednosti
+            if (string.IsNullOrEmpty(racun.BrojRacuna))
+            {
+                ModelState.AddModelError("BrojRacuna", "Broj računa je obavezan!");
+            }
+            if (string.IsNullOrEmpty(racun.Zaposlenik))
+            {
+                ModelState.AddModelError("Zaposlenik", "Zaposlenik je obavezan!");
+            }
+            if (string.IsNullOrEmpty(racun.Kupac))
+            {
+                ModelState.AddModelError("Kupac", "Kupac je obavezan!");
+            }
+
+            //model-level validacija
+            if (ModelState.IsValidField("Datum"))
+            {
+                if (racun.Datum < DateTime.Today.AddDays(-3))
+                {
+                    ModelState.AddModelError("", "Datum ne smije biti manji za više od 3 dana!");
+                }
+            }
+
+            //ukupna provjera validacije
+            if (ModelState.IsValid)
+            {
+                return View("RacunIzdan", racun);
+            }
+            else
+            {
+                if (id == "IzdavanjeRacuna2")
+                {
+                    return View("IzdavanjRacuna2");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+        }
+        public ViewResult IzdavanjeRacuna2()
+        {
+            return View(new Racun() { Datum = DateTime.Now, BrojRacuna = "/" + DateTime.Now.Year.ToString() });
+        }
+    }
+}
---
+++ validacije/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="validacije.MvcApplication" Language="C#" %>
