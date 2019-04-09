41d3e36594cf1bc152fbf392fa30f4ce4db63282
Antun Majdančić <tuna.gfos@gmail.com>
2019-04-09 16:52:46 +0200

    konekcija

---
+++ Adonet_spajanje_na_bazu/Controllers/SqlConnectionObjectController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adonet_spajanje_na_bazu.Controllers
+{
+    public class SqlConnectionObjectController : Controller
+    {
+        // GET: SqlConnectionObject
+        public ActionResult Index()
+        {
+            // Prvo connection string
+            string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AlgebraDB;Integrated Security=True";
+
+            // NAkon toga instanca Sqlconnection
+            SqlConnection conn = new SqlConnection(connString);
+            
+            try
+            {
+                conn.Open();
+                if(conn.State==System.Data.ConnectionState.Open)
+                {
+                    Response.Write("Konekcija je uspjela");
+                }
+            }
+            catch(SqlException sqex)
+            {
+                Response.Write("Greška spajanja na bazu:" + sqex.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write("Neka greška" + ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+
+
+    }
+}
